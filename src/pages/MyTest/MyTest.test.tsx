ccd004553d9395c0c72c8ad97a6c7f8b81b5f311
Ilya <misyurailya5@gmail.com>
2023-02-26 20:47:36 +0200

    simple tests

--- src/pages/MyTest/MyTest.test.tsx
+++ src/pages/MyTest/MyTest.test.tsx
@@ -1,17 +1,51 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom'
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom/extend-expect';
 import MyTest from './MyTest';
 
-describe('TEST Component', () => {
+describe('TEST APP', () => {
   test('renders learn react link', () => {
     render(<MyTest />);
-    const linkElement = screen.getByText(/hello world/i);
+    const helloWorldElem = screen.getByText(/hello world/i)
     const btn = screen.getByRole('button')
     const input = screen.getByPlaceholderText(/input value/i)
-    expect(linkElement).toBeInTheDocument();
+    expect(helloWorldElem).toBeInTheDocument();
     expect(btn).toBeInTheDocument();
-    expect(input).toBeInTheDocument();
     expect(input).toMatchSnapshot();
   });
+
+  test('renders learn react link', async () => {
+    render(<MyTest />);
+    // const helloWorldElem = screen.queryByText(/hello2/i)
+    // expect(helloWorldElem).toBeNull()
+    screen.debug()
+    const helloWorldElem = await screen.findByText(/data/i)
+    expect(helloWorldElem).toBeInTheDocument();
+    expect(helloWorldElem).toHaveStyle({color: 'red'});
+    screen.debug()
+  });
+
+  test("CLICK EVENT", () => {
+    render(<MyTest />);
+    const btn = screen.getByTestId('toggle-btn');
+    expect(screen.queryByTestId('toggle-elem')).toBeNull();
+    fireEvent.click(btn)
+    expect(screen.queryByTestId('toggle-elem')).toBeInTheDocument();
+    fireEvent.click(btn)
+    expect(screen.queryByTestId('toggle-elem')).toBeNull();
+  })
+
+  test("INPUT EVENT", () => {
+    render(<MyTest />);
+    const input = screen.getByPlaceholderText(/input value/i)
+    expect(screen.queryByTestId('value-elem')).toContainHTML('');
+    // Искуственное событие
+    fireEvent.input(input, {
+      target: {value: '123123'}
+    })
+    // Близко к пользователю, обрабатываются события нажатия клавиш и тд
+    // userEvent.type(input, '123123')
+    expect(screen.queryByTestId('value-elem')).toContainHTML('123123');
+  })
 })
--- src/pages/MyTest/MyTest.tsx
+++ src/pages/MyTest/MyTest.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-confusing-void-expression */
+/* eslint-disable @typescript-eslint/key-spacing */
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import React, { useEffect, useState } from 'react';
 
 const MyTest = () => {
+    const [data, setData] = useState('')
+    const [toggle, setToggle] = useState(false);
+    const [value, setValue] = useState('');
+
+    const onClick = () => setToggle(prev => !prev);
+
+    useEffect(() => {
+        setTimeout(() => {
+            return setData('ad');
+        }, 100)
+    }, [])
     return (
         <div>
+            <h1 data-testid="value-elem">{value}</h1>
+            {toggle && <div data-testid="toggle-elem">toggle</div>}
+            {data && <div style={{color: 'red'}}>data</div>}
             <h1>Hello world</h1>
-            <input placeholder='input value' type='text'/>
-            <button>click me</button>
+            <button data-testid="toggle-btn" onClick={onClick}>click me</button>
+            <input onChange={e => setValue(e.target.value)} type="text" placeholder="input value....."/>
         </div>
     );
 };
